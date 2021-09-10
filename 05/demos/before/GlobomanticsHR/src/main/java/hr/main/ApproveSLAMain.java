c88731986d99e39faeab570990563f8f8310f11e
bra0144 <bran0144@umn.edu>
2021-09-10 16:13:46 -0500

    follow Liskov substitution principle

--- 05/demos/before/GlobomanticsHR/src/main/java/hr/main/ApproveSLAMain.java
+++ 05/demos/before/GlobomanticsHR/src/main/java/hr/main/ApproveSLAMain.java
@@ -7,30 +7,28 @@ import hr.personnel.Employee;
 import hr.personnel.ServiceLicenseAgreement;
 import hr.personnel.Subcontractor;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class ApproveSLAMain {
     public static void main(String[] args) {
-        // Create dependencies
-        ConsoleLogger consoleLogger = new ConsoleLogger();
-        EmployeeFileSerializer employeeFileSerializer = new EmployeeFileSerializer();
-        EmployeeRepository repository = new EmployeeRepository(employeeFileSerializer);
-
-        // Define SLA
-        int minTimeOffPercent = 98;
+        int minTimeOffPerfect = 98;
         int maxResolutionDays = 2;
-        ServiceLicenseAgreement companySla = new ServiceLicenseAgreement(
-                minTimeOffPercent,
-                maxResolutionDays);
+        ServiceLicenseAgreement companySla = new ServiceLicenseAgreement(minTimeOffPerfect, maxResolutionDays);
 
-        // Grab subcontractors
-        List<Employee> subcontractors = repository.findAll();
-
-        for (Employee e : subcontractors){
-            if(e instanceof  Subcontractor){
-                Subcontractor s = (Subcontractor)e;
-                s.approveSLA(companySla);
-            }
+        Subcontractor subcontractor1 = new Subcontractor(
+                "Rebekah Jackson",
+                "rebekah-jackson@abc.com",
+                3000,
+                15);
+        Subcontractor subcontractor2 = new Subcontractor(
+                "Harry Smith",
+                "harry-smith@abc.com",
+                3000,
+                15);
+        List<Subcontractor> collaborators = Arrays.asList(subcontractor1, subcontractor2);
+        for (Subcontractor s : collaborators) {
+            s.approveSLA(companySla);
         }
     }
 }
